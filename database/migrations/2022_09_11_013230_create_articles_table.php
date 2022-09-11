558c8c8abc376be176b86516f45bd60ca9a5257a
velmouth-star <shanpuyangshu@gmail.com>
2022-09-11 10:46:05 +0900

    sail artisan db:seed

--- database/migrations/2022_09_11_013230_create_articles_table.php
+++ database/migrations/2022_09_11_013230_create_articles_table.php
@@ -14,7 +14,9 @@ class CreateArticlesTable extends Migration
     public function up()
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
